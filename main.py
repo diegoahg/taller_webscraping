9f923f7255a79812602228082ddc8fc82b7e6684
Diego Hernandez Garcia <dahernandezg@sodimac.cl>
2022-06-23 15:00:27 -0400

    Webscraping with BeautifulSoup

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 # Encontrar todos los div que tienen class 'item-hito'
 divs = soup.find_all('div', {'class':['col-md-6']})
-i=0
 for div in divs:
-    # find all the enclosing a tags.
-    if(i == 0):
-        print(div)
-    i = i + 1
+    text = div.find_all('p', {'class':['testimony-text']})
+    print(text[0])
